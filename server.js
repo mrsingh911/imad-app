41964797b8ffd25fb3d03705bb4eaaae87c0a54a
mrsingh911 <30700667+mrsingh911@users.noreply.github.com>
2017-08-04 23:17:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 app.get('/article-one', function(req,res)
 {
-    res.send('This will serve article one');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 }
 );
 
