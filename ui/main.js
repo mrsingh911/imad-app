a07067453de0240b9d62dbffb7664a4700de0a3b
mrsingh911 <30700667+mrsingh911@users.noreply.github.com>
2017-08-08 04:03:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,8 +33,7 @@ button.onclick = function()
 
 //Submit name 
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function()
 {
@@ -67,7 +66,8 @@ submit.onclick = function()
         //Not done yet.
     };
    //Make the request
-   
+   var nameInput = document.getElementById('name');
+    var name = nameInput.value;
    request.open('GET','navjit911.imad.hasura-app.io/submit-name?name='+ name,true);
    request.send(null);
 
